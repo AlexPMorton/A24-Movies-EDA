5c98c68956d5bb2815ac3f615e3c77603cd41f7d
Alex <alexpaulmorton@gmail.com>
2022-09-26 23:21:33 -0700

    Updated CSV files and names and worked on genres analysis section

--- SQL Queries/A24_Oscars_Table.sql
+++ SQL Queries/A24_Oscars_Table.sql
@@ -1,6 +1,6 @@
 SELECT title, release_date, genre, director, budget, domestic_revenue,
 international_revenue, worldwide_revenue, runtime, mpaa_rating,
-CASE WHEN film IS NOT NULL AND film <> 'Skin' THEN 'YES' ELSE NULL END AS oscar_win
+CASE WHEN film IS NOT NULL AND film <> 'Skin' THEN 'YES' ELSE 'NO' END AS oscar_win
 FROM (
 	SELECT * 
 	FROM a24_released_movies arm 
